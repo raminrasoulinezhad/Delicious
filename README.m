60ee99d53eb1a761800b03d0e8be6b07aab9f07e
SeyedRamin Rasoulinezhad <ramin_r348@yahoo.com>
2019-09-19 13:30:11 +1000

    Create README.m

---
+++ README.m
@@ -0,0 +1,19 @@
+# Screen
+  
+  
+  screen            ---> to creat 
+  screan ~r <name>  ---> to reconnect
+  screan ~rls       ---> to list the screens
+  
+  cntr + AD         ---> temp close
+  cntr + AK         ---> kill
+  
+  cntr + AN         ---> next screen
+  cntr + AS         ---> splite
+  
+  
+# Vim
+
+  u           ---> undo
+  cntr + r    ---> redo
+  
