3c4ddab98529e809c3329818bd8fee7f18a77ca3
SeyedRamin Rasoulinezhad <ramin_r348@yahoo.com>
2020-01-17 13:52:05 +1100

    Update README.m

--- README.m
+++ README.m
@@ -21,3 +21,17 @@
 1- find the PID with htop
 2- use ps aux to find out the screen/shell which is assigned to the command
 3- use who to see who is the owner of the screen/shell
+
+
+# Dell Fan controller
+https://www.cyberciti.biz/faq/controlling-dell-fan-speeds-temperature-on-ubuntu-debian-linux/
+
+Automatic:
+  /etc/init.d/i8kmon start
+Manual:
+   i8kctl fan 2 2 
+ 
+0 : turn the fan off (not recommended)
+1 : set low speed
+2 : set high speed
+- : don’t change the state of this fan
